496f898128ee644b503e228885ec4944f2a63b09
Ankit Vishwakarma <ankit99aug@outlook.com>
2024-06-01 15:05:23 +0530

    social media app added

--- Social Media App/server/index.js
+++ Social Media App/server/index.js
@@ -1,19 +1,26 @@
-import express from "express"
-import bodyParser  from "body-parser"
-import mongoose from "mongoose"
-import cors from 'cors'
-import postRoutes from './routes/posts.js'
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
+dotenv.config();
 const app = express();
 
-app.use(bodyParser({limit:'30mb',extended:true}))
-// app.use(bodyParser.urlencoded({limit:'30mb',extended:true}))
-app.use(cors())
-const CONNECTION_URL ="mongodb+srv://ankit99aug:root@cluster0.hxf156l.mongodb.net/"
-const PORT = process.env.PORT || 5000
-mongoose.connect(CONNECTION_URL)
-.then(()=>{
-    app.listen(PORT,()=>console.log(`Server running on ${PORT}`))
-}).catch((err)=>console.log(err.message));
-app.use('/posts',postRoutes);
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
 
-mongoose.set();
+app.use('/posts', postRoutes);
+app.use("/user", userRouter);
+
+const CONNECTION_URL = 'mongodb+srv://js_mastery:M6WfDnJEoj9HkV2d@practice.jto9p.mongodb.net/memories_app?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
